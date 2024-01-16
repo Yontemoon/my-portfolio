d52adbc2b79a63cd1207e61d46b37e18407c2b07
apex-devteam <devteam@apexpostech.com>
2024-01-16 06:39:51 -0800

    Testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Navbar from '@/ui/Navbar'
 import Image from 'next/image'
 
-export default function Home({children}: {children: React.ReactNode}) {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Navbar/>
-      {children}
+      Home page
     </main>
   )
 }
