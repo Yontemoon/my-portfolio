5e83953aad05e91e9c01060041cfec4892516d5f
apex-devteam <devteam@apexpostech.com>
2024-04-22 12:12:57 -0700

    Fixed module not found error

--- app/components/MaxProjectsPage.tsx
+++ app/components/MaxProjectsPage.tsx
@@ -1,7 +1,7 @@
 import ProjectLink from "./ProjectLink";
 import templateImg from "../images/template-image.jpg"
 import ProjectIcon from "../icons/ProjectIcon";
-import MovieNextjs from "../images/movie-nextjs.png"
+import MovieNextjs from "../images/movie-nextjs.jpg"
 
 const MaxProjectsPage = () => {
 
@@ -17,36 +17,13 @@ const MaxProjectsPage = () => {
             <div className="flex flex-col items-center p-4">
                 <ProjectLink 
                     title="Monte Movies - Movie Tracker"
-                    project_description="Application to search for movies. Add to movies to your own watchlist, rate movies, and add them to your favorites. Used Third Party API (TMDb)."
+                    project_description="Application to search for movies. Add movies to your own watchlist, rate movies, and add them to your favorites. Used Third Party API (TMDb)."
                     image={MovieNextjs}
                     descriptors={["Next.js", "Authentication", "Frontend", "API"]}
                     link_to_case="https://github.com/Yontemoon/movie-nextjs"
                     link_to_site="https://movie-nextjs-chi.vercel.app/"
                 />
-                <ProjectLink 
-                    title={"Movie List"}
-                    project_description="Application to search for movies to add onto your watchlist or favorite it if you've already watched it."
-                    image={templateImg}
-                    descriptors={["React", "Web Design", "Full Stack"]}
-                    link_to_case="/case-movie-app"
-                    link_to_site="youtube.com"
-                />
-                <ProjectLink 
-                    title={"Movie List"}
-                    project_description="Application to search for movies to add onto your watchlist or favorite it if you've already watched it."
-                    image={templateImg}
-                    descriptors={["React", "Web Design", "Full Stack"]}
-                    link_to_case="/case-movie-app"
-                    link_to_site="youtube.com"
-                />
-                <ProjectLink 
-                    title={"Movie List"}
-                    project_description="Application to search for movies to add onto your watchlist or favorite it if you've already watched it."
-                    image={templateImg}
-                    descriptors={["React", "Web Design", "Full Stack"]}
-                    link_to_case="/case-movie-app"
-                    link_to_site="youtube.com"
-                />
+                
             </div>
             {/* <p className="text-start">
                 View My Résumé
