f4942d269300baf614f31eff48ed0875ceb6e49a
YonteMoon <monteyoon1998@gmail.com>
2024-08-06 23:27:15 -0700

    Added robot and sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://monteyoon.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://monteyoon.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://monteyoon.com/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://monteyoon.com/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
